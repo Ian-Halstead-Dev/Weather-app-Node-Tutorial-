26eaecbf5f3932b2045e984e783c617f65d255c5
Ian Halstead <iwhalstead10@gmail.com>
2020-04-30 17:00:11 -0600

    Made Error message close after 5 seconds

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,14 +1,21 @@
-const closeBtn = document.getElementById('close-alert');
+const closeAlert = document.getElementById('close-alert');
 const alertContainer = document.getElementById('alert');
 const alertMessage = document.getElementById('alert-message');
 const alertTitle = document.getElementById('alert-title');
 
-closeBtn.addEventListener('click', () => {
+closeAlert.addEventListener('click', () => {
 	alertContainer.style.display = 'none';
+	window.clearTimeout(alertTimeout);
 });
+
+// Allows me to reset the timer when the closeAlert is clicked
+let alertTimeout = undefined;
 
 const alertHTML = (title, message) => {
 	alertMessage.innerText = message;
 	alertTitle.innerText = title;
 	alertContainer.style.display = 'flex';
+	alertTimeout = window.setTimeout(() => {
+		alertContainer.style.display = 'none';
+	}, 5000);
 };
